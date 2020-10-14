554e66ddb85bb303d023c87e3e9c50c57370ffa4
Emir Ensar <64320256+rahmanlar@users.noreply.github.com>
2020-10-14 21:04:51 +0300

    Add files via upload

---
+++ ferdateam/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ferdateam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ferda Hoca odev sorgulama sistemi";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Sence Ferda Hoca bugun odev verdi mi?");
+            string verdimi = Console.ReadLine();
+            Console.WriteLine("Cevabin " + verdimi + " bakalim dogru mu?");
+
+            Console.WriteLine("Ferda Hoca bugun odev vermedi.");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey(); 
+        }
+    }
+}
