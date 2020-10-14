91dce9251d162f09bb31026325b933b81d5f67c7
Emir Ensar <64320256+rahmanlar@users.noreply.github.com>
2020-10-14 21:11:04 +0300

    Edit colors

--- ferdateam/Program.cs
+++ ferdateam/Program.cs
@@ -9,9 +9,10 @@ namespace ferdateam
             Console.Title = "Ferda Hoca odev sorgulama sistemi";
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Sence Ferda Hoca bugun odev verdi mi?");
+            Console.ForegroundColor = ConsoleColor.White;
             string verdimi = Console.ReadLine();
             Console.WriteLine("Cevabin " + verdimi + " bakalim dogru mu?");
-
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Ferda Hoca bugun odev vermedi.");
             Console.ForegroundColor = ConsoleColor.White;
             Console.ReadKey(); 
