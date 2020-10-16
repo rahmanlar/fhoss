b235e74381623d0a044cf02e18a0aa359a24a32d
Emir Ensar <64320256+rahmanlar@users.noreply.github.com>
2020-10-16 23:02:09 +0300

    Ferda Teacher now gives homework on Fridays.

--- ferdateam/Program.cs
+++ ferdateam/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
-
+using System;
 namespace ferdateam
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DateTime tarih = DateTime.Now;
             Console.Title = "Ferda Hoca odev sorgulama sistemi";
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Sence Ferda Hoca bugun odev verdi mi?");
@@ -13,7 +13,10 @@ namespace ferdateam
             string verdimi = Console.ReadLine();
             Console.WriteLine("Cevabin " + verdimi + " bakalim dogru mu?");
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Ferda Hoca bugun odev vermedi.");
+        if (tarih.DayOfWeek.ToString() == "Friday")
+            Console.WriteLine("Ferda Hoca bugun odev verdi. Evet. Maalesef.");
+        else 
+            Console.WriteLine("Ferda Hoca bugun odev vermedi. Yasasin!");
             Console.ForegroundColor = ConsoleColor.White;
             Console.ReadKey(); 
         }
